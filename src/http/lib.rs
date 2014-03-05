3090259ab1c5678d2f4beb9fd5b3b9f0f5765360
Andelf <andelf@gmail.com>
2014-03-05 18:03:20 +0800

    refactor to a rust lib

--- src/http/lib.rs
+++ src/http/lib.rs
@@ -1,3 +1,11 @@
+#[desc = "A rust crate for http protocol"];
+#[license = "MIT"];
+
+#[crate_id = "http#0.1-pre"];
+#[crate_type = "rlib"];
+#[crate_type = "dylib"];
+
+
 #[allow(unused_must_use)];
 #[allow(dead_code)];
 
@@ -22,18 +30,19 @@ use std::ascii::StrAsciiExt;
 //use std::ascii::AsciiStr;
 use std::num::from_str_radix;
 
-use extra::url::{Url, query_to_str};
+//use extra::url::{Url, query_to_str};
+pub use urlencode = extra::url::query_to_str;
+pub use extra::url::Url;
 
 use collections::HashMap;
 
-use cookie::Cookie;
-pub use urlencode = extra::url::query_to_str;
+pub use cookie::Cookie;
 
 pub mod cookie;
 
+
 static USER_AGENT : &'static str = "Rust-http-helper/0.1dev";
 static HTTP_PORT : u16 = 80;
-
 
 #[deriving(Show)]
 pub enum HttpMethod {
@@ -97,8 +106,7 @@ impl<'a> Request<'a> {
 // fn header_eq(a: &str, b: &str) -> bool {
 //     a.eq_ignore_ascii_case(b)
 // }
-
-fn to_header_case(key: &str) -> ~str {
+pub fn to_header_case(key: &str) -> ~str {
     let mut ret = ~"";
     let mut flag_is_at_words_begin = true;
     for c in key.as_bytes().iter() {
@@ -181,7 +189,7 @@ impl Handler for HTTPHandler {
 
         if !uri.query.is_empty() {
             stream.write_char('?');
-            stream.write_str(query_to_str(&uri.query));
+            stream.write_str(urlencode(&uri.query));
         }
 
         stream.write_str(" ");
@@ -451,191 +459,4 @@ impl<'a> Reader for Response<'a> {
             self.sock.read(buf)
         }
     }
-}
-
-fn main() {
-    //let u = ~"http://flash.weather.com.cn/sk2/101010100.xml";
-    let u = ~"http://zx.bjmemc.com.cn/ashx/Data.ashx?Action=GetAQIClose1h";
-    //let u = "http://www.gpsspg.com/ajax/latlng_office_maps.aspx?lat=39.91433268343&lng=116.46717705386&type=1";
-    //req.add_header("Referer", "http://www.gpsspg.com/apps/maps/google_131201.htm");
-
-    let url : Url = from_str(u).unwrap();
-    let mut req = Request::new_with_url(&url);
-    //req.method = POST;
-
-    //req.add_header("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3");
-
-    let mut h = HTTPHandler { debug: true };
-    let mut resp = h.handle(&mut req).unwrap();
-
-    dump_result(&req, &resp);
-
-    match resp.read_to_end() {
-        Ok(content) => {
-            for c in content.iter() {
-                print!("{:c}", *c as char);
-            }
-            println!("DEBUG read bytes=> {}", content.len());
-        }
-        Err(e) => {
-            println!("! read error: {:?}", e);
-        }
-    }
-    println!("get_headers = {:?}", resp.get_headers("set-cookie"));
-}
-
-
-fn dump_result(req: &Request, resp: &Response) {
-    println!("\n======================= request result =======================");
-    for (k, vs) in req.headers.iter() {
-        println!("H {:?} => {:?}", k, vs)
-    }
-
-    println!("======================= response result =======================");
-    println!("status = {} reason = {}", resp.status, resp.reason);
-    for (k, vs) in resp.headers.iter() {
-        println!("H {:?} => {:?}", k, vs)
-    }
-}
-
-
-#[test]
-fn test_cookie_parse() {
-    let url = from_str("http://www.baidu.com/").unwrap();
-    let mut req = Request::new_with_url(&url);
-    let mut h = HTTPHandler { debug : true };
-    let mut resp = h.handle(&mut req).unwrap();
-
-    let mut cj = CookieJar::new();
-    dump_result(&req, &resp);
-    for set_ck in resp.headers.get(&to_header_case("set-cookie")).iter() {
-        let ck_opt = from_str::<Cookie>(*set_ck);
-        assert!(ck_opt.is_some());
-        let ck = ck_opt.unwrap();
-
-        //println!("got cookie => {:?}", ck);
-        info!("expired => {:?}", ck.is_expired());
-        println!("header req => {:?}", ck.to_header());
-        //println!("header_str => {:?}", ck.to_str());
-        cj.set_cookie_if_ok(ck, &req);
-    }
-    assert!(cj.cookies_for_request(&req).len() > 0);
-    println!("CJ => {:?}", cj);
-    assert_eq!(resp.status, 200);
-}
-
-#[test]
-fn test_post_request() {
-    let url = from_str("http://202.118.8.2:8080/book/queryOut.jsp").unwrap();
-    let mut req = Request::new_with_url(&url);
-    let mut h = HTTPHandler { debug : true };
-
-    req.method = POST;
-    req.add_body(bytes!("kind=simple&type=title&word=erlang&match=mh&recordtype=01&library_id=all&x=40&y=10"));
-
-    let mut resp = h.handle(&mut req).unwrap();
-
-    dump_result(&req, &resp);
-    // charset = gbk
-    match resp.read_to_end() {
-        Ok(content) => {
-            for c in content.iter() {
-                print!("{:c}", *c as char);
-            }
-            println!("DEBUG read bytes=> {}", content.len());
-        }
-        Err(e) => {
-            println!("! read error: {:?}", e);
-        }
-    }
-    assert_eq!(resp.status, 200);
-}
-
-#[test]
-fn test_options_request() {
-    let url = from_str("http://www.w3.org").unwrap();
-    let mut req = Request::new_with_url(&url);
-    req.method = OPTIONS;
-    let mut h = HTTPHandler { debug : true };
-    let mut resp = h.handle(&mut req).unwrap();
-
-    assert_eq!(resp.status, 200);
-    assert!(resp.headers.find(&~"Allow").is_some());
-}
-
-
-
-//#[test]
-// fn test_gzip_uncompress() {
-//     let url = from_str("http://www.baidu.com").unwrap();
-//     let mut req = Request::new_with_url(&url);
-//     req.add_header("Accept", "*/*");
-//     req.add_header("Accept-Encoding", "gzip,deflate,sdch");
-//     req.add_header("User-Agent", "Mozilla/5.0");
-//     let mut h = HTTPHandler { debug : true };
-//     let mut resp = h.handle(&mut req).unwrap();
-
-//     dump_result(&req, &resp);
-//     let content = resp.read_to_end().unwrap();
-//     //println!("| {:?}", content);
-//     println!("|uncompress => {:?}", compress::zlib_uncompress(content.slice(10, content.len()-8)));
-
-
-//     assert_eq!(resp.status, 200);
-//     assert!(false);
-
-// }
-
-
-
-#[test]
-fn test_head_request() {
-    let url = from_str("http://www.w3.org").unwrap();
-    let mut req = Request::new_with_url(&url);
-    req.method = HEAD;
-    let mut h = HTTPHandler { debug : true };
-    let mut resp = h.handle(&mut req).unwrap();
-
-    assert_eq!(resp.read_to_end().unwrap().len(), 0);
-    assert_eq!(resp.status, 200);
-}
-
-#[test]
-fn test_yahoo_redirect_response() {
-    let url = from_str("http://www.yahoo.com.cn").unwrap();
-    let mut req = Request::new_with_url(&url);
-    //req.headers.find_or_insert(~"Accept-Encoding", ~[~"gzip,deflate,sdch"]);
-
-    let mut h = HTTPHandler { debug: true };
-    let mut resp = h.handle(&mut req).unwrap();
-
-    assert_eq!(resp.status, 301);
-}
-
-
-#[test]
-fn test_weather_sug() {
-    let url : Url = from_str("http://toy1.weather.com.cn/search?cityname=yulin&_=2").unwrap();
-
-    let mut req = Request::new_with_url(&url);
-    req.headers.find_or_insert(~"Referer", ~[~"http://www.weather.com.cn/"]);
-
-    let mut h = HTTPHandler { debug: true };
-    let mut resp = h.handle(&mut req).unwrap();
-
-    let content = match resp.read_to_str() {
-        Ok(content) => {
-            content
-        }
-        Err(_) =>
-            ~""
-    };
-    assert!(content.len() > 10);
-    assert!(resp.status == 200);
-}
-
-#[test]
-fn test_header_case() {
-    assert_eq!(to_header_case("X-ForWard-For"), ~"X-Forward-For");
-    assert_eq!(to_header_case("accept-encoding"), ~"Accept-Encoding");
 }
---
+++ src/http/test.rs
@@ -0,0 +1,161 @@
+#[feature(globs)];
+#[allow(unused_mut)];
+
+extern crate http;
+
+use http::*;
+
+fn dump_result(req: &Request, resp: &Response) {
+    println!("\n======================= request result =======================");
+    for (k, vs) in req.headers.iter() {
+        println!("H {:?} => {:?}", k, vs)
+    }
+
+    println!("======================= response result =======================");
+    println!("status = {} reason = {}", resp.status, resp.reason);
+    for (k, vs) in resp.headers.iter() {
+        println!("H {:?} => {:?}", k, vs)
+    }
+}
+
+
+#[test]
+fn test_cookie_parse() {
+    let url = from_str("http://www.baidu.com/").unwrap();
+    let mut req = Request::new_with_url(&url);
+    let mut h = HTTPHandler { debug : true };
+    let mut resp = h.handle(&mut req).unwrap();
+
+    let mut cj = CookieJar::new();
+    dump_result(&req, &resp);
+    for set_ck in resp.headers.get(&to_header_case("set-cookie")).iter() {
+        let ck_opt = from_str::<Cookie>(*set_ck);
+        assert!(ck_opt.is_some());
+        let ck = ck_opt.unwrap();
+
+        //println!("got cookie => {:?}", ck);
+        info!("expired => {:?}", ck.is_expired());
+        println!("header req => {:?}", ck.to_header());
+        //println!("header_str => {:?}", ck.to_str());
+        cj.set_cookie_if_ok(ck, &req);
+    }
+    assert!(cj.cookies_for_request(&req).len() > 0);
+    println!("CJ => {:?}", cj);
+    assert_eq!(resp.status, 200);
+}
+
+#[test]
+fn test_post_request() {
+    let url = from_str("http://202.118.8.2:8080/book/queryOut.jsp").unwrap();
+    let mut req = Request::new_with_url(&url);
+    let mut h = HTTPHandler { debug : true };
+
+    req.method = POST;
+    req.add_body(bytes!("kind=simple&type=title&word=erlang&match=mh&recordtype=01&library_id=all&x=40&y=10"));
+
+    let mut resp = h.handle(&mut req).unwrap();
+
+    dump_result(&req, &resp);
+    // charset = gbk
+    match resp.read_to_end() {
+        Ok(content) => {
+            for c in content.iter() {
+                print!("{:c}", *c as char);
+            }
+            println!("DEBUG read bytes=> {}", content.len());
+        }
+        Err(e) => {
+            println!("! read error: {:?}", e);
+        }
+    }
+    assert_eq!(resp.status, 200);
+}
+
+#[test]
+fn test_options_request() {
+    let url = from_str("http://www.w3.org").unwrap();
+    let mut req = Request::new_with_url(&url);
+    req.method = OPTIONS;
+    let mut h = HTTPHandler { debug : true };
+    let mut resp = h.handle(&mut req).unwrap();
+
+    assert_eq!(resp.status, 200);
+    assert!(resp.headers.find(&~"Allow").is_some());
+}
+
+
+
+//#[test]
+// fn test_gzip_uncompress() {
+//     let url = from_str("http://www.baidu.com").unwrap();
+//     let mut req = Request::new_with_url(&url);
+//     req.add_header("Accept", "*/*");
+//     req.add_header("Accept-Encoding", "gzip,deflate,sdch");
+//     req.add_header("User-Agent", "Mozilla/5.0");
+//     let mut h = HTTPHandler { debug : true };
+//     let mut resp = h.handle(&mut req).unwrap();
+
+//     dump_result(&req, &resp);
+//     let content = resp.read_to_end().unwrap();
+//     //println!("| {:?}", content);
+//     println!("|uncompress => {:?}", compress::zlib_uncompress(content.slice(10, content.len()-8)));
+
+
+//     assert_eq!(resp.status, 200);
+//     assert!(false);
+
+// }
+
+
+
+#[test]
+fn test_head_request() {
+    let url = from_str("http://www.w3.org").unwrap();
+    let mut req = Request::new_with_url(&url);
+    req.method = HEAD;
+    let mut h = HTTPHandler { debug : true };
+    let mut resp = h.handle(&mut req).unwrap();
+
+    assert_eq!(resp.read_to_end().unwrap().len(), 0);
+    assert_eq!(resp.status, 200);
+}
+
+#[test]
+fn test_yahoo_redirect_response() {
+    let url = from_str("http://www.yahoo.com.cn").unwrap();
+    let mut req = Request::new_with_url(&url);
+    //req.headers.find_or_insert(~"Accept-Encoding", ~[~"gzip,deflate,sdch"]);
+
+    let mut h = HTTPHandler { debug: true };
+    let mut resp = h.handle(&mut req).unwrap();
+
+    assert_eq!(resp.status, 301);
+}
+
+
+#[test]
+fn test_weather_sug() {
+    let url : Url = from_str("http://toy1.weather.com.cn/search?cityname=yulin&_=2").unwrap();
+
+    let mut req = Request::new_with_url(&url);
+    req.headers.find_or_insert(~"Referer", ~[~"http://www.weather.com.cn/"]);
+
+    let mut h = HTTPHandler { debug: true };
+    let mut resp = h.handle(&mut req).unwrap();
+
+    let content = match resp.read_to_str() {
+        Ok(content) => {
+            content
+        }
+        Err(_) =>
+            ~""
+    };
+    assert!(content.len() > 10);
+    assert!(resp.status == 200);
+}
+
+#[test]
+fn test_header_case() {
+    assert_eq!(to_header_case("X-ForWard-For"), ~"X-Forward-For");
+    assert_eq!(to_header_case("accept-encoding"), ~"Accept-Encoding");
+}
