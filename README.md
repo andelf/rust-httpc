aa70457f472d56ce281d43d8486365d4e1dfbd24
Andelf <andelf@gmail.com>
2014-03-07 02:03:56 +0800

    README: add simple intro, add usage sample

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-working in progress & proof of concept
+# rust-http-helper
+
+## A http client lib.
+
+- Working in progress.
+- Proof of concept.
+- Code needs clean up.
+
+## Support Following:
+
+- fundamental HTTP/1.0 HTTP/1.1 (partial)
+- HTTP method: GET/POST/PUT ...
+- HTTP headers (set/get)
+- HTTP chunked transfer encoding
+- simple HTTP CookieJar
+- HTTP gzip/deflate content encoding (partial, WIP, via zlib)
+- a Opener + Handler structure, extensible (like python urllib2)
+
+## What is missing
+
+- keep-alive support
+- multipart support
+- sdch support (no plan)
+- redirect support
+- cookie persistence
+- timing
+- logger
+- ... and so on
+
+# Simple Usage
+
+```rust
+#[feature(globs)];
+
+extern crate http;
+use http::*;
+
+fn dump_result(req: &Request, resp: &Response) {
+    println!("\n======================= request result =======================");
+    for (k, vs) in req.headers.iter() {
+        println!("H {:?} => {:?}", k, vs)
+    }
+    println!("======================= response result =======================");
+    println!("status = {} reason = {}", resp.status, resp.reason);
+    for (k, vs) in resp.headers.iter() {
+        println!("H {:?} => {:?}", k, vs)
+    }
+}
+
+fn main() {
+    let url : Url = from_str("http://www.google.com").unwrap();
+    let mut req = Request::new_with_url(&url);
+
+    // // Support Following:
+    //req.add_header("Accept-Encoding", "gzip,deflate");
+    //req.method = POST;
+    //req.add_header("user-agent", "Mozilla/5.0");
+
+    let mut opener = build_opener();
+    let mut resp = opener.open(&mut req).unwrap();
+
+    dump_result(&req, &resp);
+
+    println!("got cookies = {:?}", resp.get_headers("set-cookie"));
+    match resp.get_headers("Content-Encoding").head() {
+        Some(&~"gzip") => {
+            let mut gzreader = compress::GzipReader::new(resp);
+            println!("unzip => {:?}",  gzreader.read_to_str());
+        }
+        Some(&~"deflate") => {
+            let mut gzreader = compress::GzipReader::new(resp);
+            println!("unzip => {:?}",  gzreader.read_to_str());
+        }
+        None => {
+            println!("content => {:?}", resp.read_to_str());
+        }
+        _ => unreachable!()
+    }
+}
+```
--- src/http/main.rs
+++ src/http/main.rs
@@ -1,15 +1,8 @@
 #[feature(globs)];
-#[allow(unused_mut)];
 
 extern crate http;
 
-use std::str;
 use http::*;
-
-use compress::uncompress;
-
-mod compress;
-mod zlib;
 
 
 fn dump_result(req: &Request, resp: &Response) {
@@ -27,55 +20,31 @@ fn dump_result(req: &Request, resp: &Response) {
 
 
 fn main() {
-    //let u = ~"http://flash.weather.com.cn/sk2/101010100.xml";
-    //let u = ~"http://zx.bjmemc.com.cn/ashx/Data.ashx?Action=GetAQIClose1h";
-    //let u = "http://www.gpsspg.com/ajax/latlng_office_maps.aspx?lat=39.91433268343&lng=116.46717705386&type=1";
-    //req.add_header("Referer", "http://www.gpsspg.com/apps/maps/google_131201.htm");
-    //let u = "http://www.apache.org";
-    let u = "http://www.vervestudios.co/projects/compression-tests/static/js/test-libs/jquery.min.js?d=1394076086888&format=gzip";
-    let url : Url = from_str(u).unwrap();
+    let url : Url = from_str("http://www.google.com").unwrap();
     let mut req = Request::new_with_url(&url);
 
-    req.add_header("Accept-Encoding", "gzip,deflate");
+    //req.add_header("Accept-Encoding", "gzip,deflate");
     //req.method = POST;
+    //req.add_header("user-agent", "Mozilla/5.0");
 
-    //req.add_header("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3");
-
-    let mut h = HTTPHandler { debug: true };
-    let mut resp = h.handle(&mut req).unwrap();
+    let mut opener = build_opener();
+    let mut resp = opener.open(&mut req).unwrap();
 
     dump_result(&req, &resp);
 
-    println!("get_headers = {:?}", resp.get_headers("set-cookie"));
+    println!("got cookies = {:?}", resp.get_headers("set-cookie"));
     match resp.get_headers("Content-Encoding").head() {
         Some(&~"gzip") => {
             let mut gzreader = compress::GzipReader::new(resp);
             println!("unzip => {:?}",  gzreader.read_to_str());
         }
-        Some(ce) => {
-
-            println!("content-encoding: {:?}", ce);
-
+        Some(&~"deflate") => {
             let mut gzreader = compress::GzipReader::new(resp);
             println!("unzip => {:?}",  gzreader.read_to_str());
         }
         None => {
             println!("content => {:?}", resp.read_to_str());
         }
+        _ => unreachable!()
     }
-
-/*    match resp.read_to_end() {
-        Ok(content) => {
-            let ret = compress::uncompress(content).unwrap();
-            println!("uncompress => {:?}", str::from_utf8(ret.slice_to(1000)));
-            // for c in content.iter() {
-            //     print!("{:c}", *c as char);
-            // }
-            println!("DEBUG read bytes=> {}", content.len());
-        }
-        Err(e) => {
-            println!("! read error: {:?}", e);
-        }
-    }*/
-
 }
