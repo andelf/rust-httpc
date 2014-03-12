33563f5a9a1c9234b53c58b0b040b96d2feec69b
Andelf <andelf@gmail.com>
2014-03-12 22:44:55 +0800

    compress.rs: add GzipReaderWrapper

--- src/compress.rs
+++ src/compress.rs
@@ -1,3 +1,7 @@
+/*!
+Compress library. wrapper from zlib.
+ */
+
 use std::libc::{c_char, c_int};
 use std::cast;
 use std::vec;
@@ -7,7 +11,6 @@ use std::io::IoResult;
 use std::mem::{size_of, init};
 
 mod zlib;
-
 
 static Z_OK           : c_int = 0;
 static Z_STREAM_END   : c_int = 1;
@@ -32,7 +35,7 @@ static Z_VERSION_ERROR : c_int = -6;
 
 static Z_NO_FLUSH : c_int = 0;
 
-// for http gzip/deflate
+/// for http gzip/deflate
 pub struct GzipReader<R> {
     priv inner: R,
     priv zs: zlib::z_stream,
@@ -105,6 +108,85 @@ impl<R:Reader> Reader for GzipReader<R> {
         Ok(writen)
     }
 }
+
+// duplicated code :(
+pub struct GzipReaderWrapper<'a> {
+    priv inner: &'a mut Buffer,
+    priv zs: zlib::z_stream,
+    priv buf: ~[u8],
+    priv buf_len: uint,
+    priv eof: bool,
+}
+
+
+impl<'a> GzipReaderWrapper<'a> {
+    pub fn new(r: &'a mut Buffer) -> GzipReaderWrapper {
+        static cap : uint = 65536;
+        let mut strm = unsafe { init::<zlib::z_stream>() };
+        let ret = unsafe {
+            zlib::inflateInit2_(&mut strm, 47, zlib::zlibVersion(), size_of::<zlib::z_stream>() as i32)
+        };
+        assert_eq!(ret, Z_OK);
+        let mut buf = vec::with_capacity(cap);
+        unsafe { buf.set_len(cap); }
+        GzipReaderWrapper { inner: r, // &mut r as &'a mut Reader,
+                            zs: strm, buf: buf, buf_len: 0, eof: false }
+    }
+}
+
+impl<'a> Reader for GzipReaderWrapper<'a> {
+     fn read(&mut self, buf: &mut [u8]) -> IoResult<uint> {
+        let out_len = buf.len();
+        let mut tbuf = vec::from_elem(out_len, 0u8);
+        let strm = &mut self.zs;
+
+        if self.buf_len != 0 {
+            strm.next_in = unsafe { cast::transmute(&self.buf[0]) };
+            strm.avail_in = self.buf_len as u32;
+        } else {
+           match self.inner.read(tbuf) {
+                Ok(n) if n > 0 => unsafe {
+                    strm.next_in = cast::transmute(&tbuf[0]);
+                    strm.avail_in = n as u32;
+                },
+                Ok(0)          => return Ok(0),
+                Err(e)         => {
+                    if !self.eof && e.kind == io::EndOfFile {
+                        assert_eq!(unsafe { zlib::inflateEnd(strm) }, Z_OK);
+                        self.eof = true;
+                    }
+                    return Err(e);
+                },
+                _ => unreachable!()
+            }
+        }
+        strm.next_out = unsafe { cast::transmute(&buf[0]) };
+        strm.avail_out = out_len as u32;
+
+        let ret = unsafe { zlib::inflate(strm, Z_NO_FLUSH) };
+        let writen : uint = out_len - strm.avail_out as uint;
+
+        if ret != Z_OK && ret != Z_STREAM_END { fail!("bad ret code: {}", ret) }
+
+        if strm.avail_in != 0 { // out buf too small
+            // strm.next_in will move to current ptr
+            unsafe {
+                ptr::copy_memory::<c_char>(cast::transmute(&self.buf[0]),
+                                           cast::transmute(strm.next_in),
+                                           strm.avail_in as uint);
+            }
+            self.buf_len = strm.avail_in as uint;
+        } else {
+            self.buf_len = 0;
+        }
+
+        Ok(writen)
+    }
+}
+
+
+
+
 
 #[cfg(test)]
 mod test {
