ab4eb02aceaf69b84ad76cf8fe73f53975f7403b
Andelf <andelf@gmail.com>
2014-04-17 18:17:50 +0800

    fix libc glob reexports change

--- src/httpc/zlib.rs
+++ src/httpc/zlib.rs
@@ -17,7 +17,7 @@ pub struct Struct_z_stream_s {
     pub next_out: *mut c_char,
     pub avail_out: c_uint,
     pub total_out: c_ulong,
-    pub msg: *mut c_schar,
+    pub msg: *mut c_char,
     pub state: *mut Struct_internal_state,
     pub zalloc: alloc_func,
     pub zfree: free_func,
@@ -64,7 +64,7 @@ pub struct Struct_internal_state {
 }
 #[link(name = "z")]
 extern "C" {
-    pub fn zlibVersion() -> *c_schar;
+    pub fn zlibVersion() -> *c_char;
     pub fn deflate(strm: z_streamp, flush: c_int) -> c_int;
     pub fn deflateEnd(strm: z_streamp) -> c_int;
     pub fn inflate(strm: z_streamp, flush: c_int) -> c_int;
@@ -104,15 +104,15 @@ extern "C" {
     pub fn compressBound(sourceLen: c_ulong) -> c_ulong;
     pub fn uncompress(dest: *mut c_char, destLen: *mut c_ulong, source: *c_char,
                       sourceLen: c_ulong) -> c_int;
-    pub fn gzdopen(fd: c_int, mode: *c_schar) -> gzFile;
+    pub fn gzdopen(fd: c_int, mode: *c_char) -> gzFile;
     pub fn gzbuffer(file: gzFile, size: c_uint) -> c_int;
     pub fn gzsetparams(file: gzFile, level: c_int, strategy: c_int) -> c_int;
     pub fn gzread(file: gzFile, buf: *c_void, len: c_uint) -> c_int;
     pub fn gzwrite(file: gzFile, buf: *c_void, len: c_uint) -> c_int;
-    pub fn gzprintf(file: gzFile, format: *c_schar, ...) -> c_int;
-    pub fn gzputs(file: gzFile, s: *c_schar) -> c_int;
-    pub fn gzgets(file: gzFile, buf: *mut c_schar, len: c_int) ->
-     *mut c_schar;
+    pub fn gzprintf(file: gzFile, format: *c_char, ...) -> c_int;
+    pub fn gzputs(file: gzFile, s: *c_char) -> c_int;
+    pub fn gzgets(file: gzFile, buf: *mut c_char, len: c_int) ->
+     *mut c_char;
     pub fn gzputc(file: gzFile, c: c_int) -> c_int;
     pub fn gzgetc(file: gzFile) -> c_int;
     pub fn gzungetc(c: c_int, file: gzFile) -> c_int;
@@ -123,34 +123,34 @@ extern "C" {
     pub fn gzclose(file: gzFile) -> c_int;
     pub fn gzclose_r(file: gzFile) -> c_int;
     pub fn gzclose_w(file: gzFile) -> c_int;
-    pub fn gzerror(file: gzFile, errnum: *mut c_int) -> *c_schar;
+    pub fn gzerror(file: gzFile, errnum: *mut c_int) -> *c_char;
     pub fn gzclearerr(file: gzFile);
     pub fn adler32(adler: c_ulong, buf: *c_char, len: c_uint) -> c_ulong;
     pub fn crc32(crc: c_ulong, buf: *c_char, len: c_uint) -> c_ulong;
-    pub fn deflateInit_(strm: z_streamp, level: c_int, version: *c_schar,
+    pub fn deflateInit_(strm: z_streamp, level: c_int, version: *c_char,
                         stream_size: c_int) -> c_int;
-    pub fn inflateInit_(strm: z_streamp, version: *c_schar,
+    pub fn inflateInit_(strm: z_streamp, version: *c_char,
                         stream_size: c_int) -> c_int;
     pub fn deflateInit2_(strm: z_streamp, level: c_int, method: c_int,
                          windowBits: c_int, memLevel: c_int, strategy: c_int,
-                         version: *c_schar, stream_size: c_int) -> c_int;
+                         version: *c_char, stream_size: c_int) -> c_int;
     pub fn inflateInit2_(strm: z_streamp, windowBits: c_int,
-                         version: *c_schar, stream_size: c_int) -> c_int;
+                         version: *c_char, stream_size: c_int) -> c_int;
     pub fn inflateBackInit_(strm: z_streamp, windowBits: c_int,
-                            window: *mut c_uchar, version: *c_schar,
+                            window: *mut c_uchar, version: *c_char,
                             stream_size: c_int) -> c_int;
     pub fn gzgetc_(file: gzFile) -> c_int;
-    pub fn gzopen(arg1: *c_schar, arg2: *c_schar) -> gzFile;
+    pub fn gzopen(arg1: *c_char, arg2: *c_char) -> gzFile;
     pub fn gzseek(arg1: gzFile, arg2: off_t, arg3: c_int) -> off_t;
     pub fn gztell(arg1: gzFile) -> off_t;
     pub fn gzoffset(arg1: gzFile) -> off_t;
     pub fn adler32_combine(arg1: c_ulong, arg2: c_ulong, arg3: off_t) -> c_ulong;
     pub fn crc32_combine(arg1: c_ulong, arg2: c_ulong, arg3: off_t) -> c_ulong;
-    pub fn zError(arg1: c_int) -> *c_schar;
+    pub fn zError(arg1: c_int) -> *c_char;
     pub fn inflateSyncPoint(arg1: z_streamp) -> c_int;
     // pub fn get_crc_table() -> *z_crc_t;
     pub fn inflateUndermine(arg1: z_streamp, arg2: c_int) -> c_int;
     pub fn inflateResetKeep(arg1: z_streamp) -> c_int;
     pub fn deflateResetKeep(arg1: z_streamp) -> c_int;
-    // pub fn gzvprintf(file: gzFile, format: *c_schar, va: va_list) -> c_int;
+    // pub fn gzvprintf(file: gzFile, format: *c_char, va: va_list) -> c_int;
 }
